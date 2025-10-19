9060fadca978b7c1ceded9ca16104a2ab998beca
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-10-19 12:21:35 +0000

    "resolvido"

--- 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
+++ 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
@@ -1,4 +1,21 @@
 // Antes de iniciar, instale o prompt-sync para entrada de dados e o jest para testar
+const prompt = require("prompt-sync")()
 // Comando: npm i prompt-sync; npm i -D jest
-
 // CRIE UMA LÓGICA ABAIXO
+let pecas = 1
+let num = prompt(`Quantas peças devem ser produzidas?: `)
+
+while (pecas <= num ) {
+    console.log(`Peça ${pecas} produzida.`)
+    pecas ++
+}
+
+if (pecas = num) {
+    console.log(`Meta alcançada!`)
+} else {
+    console.log(`Meta não alcançada.`)
+}
+
+do {
+    let continuar = prompt(`Deseja simular outro turno? (S/N)`)
+} while
