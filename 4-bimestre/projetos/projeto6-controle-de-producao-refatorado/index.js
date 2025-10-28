9cefc9017dcefd6f54d94f9741f5707fd81e775a
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-10-28 21:20:01 +0000

    resolvido!

--- 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
+++ 4-bimestre/projetos/projeto6-controle-de-producao-refatorado/index.js
@@ -1,1 +1,48 @@
 // CRIE UMA LÓGICA ABAIXO
+const prompt = require("prompt-sync")()
+
+let num = 1
+let dias = parseInt(prompt(`Quantos dias serão registrados? `))
+let pecas
+let producao = []
+while (num <= dias) {
+    pecas = parseInt(prompt(`Quantas peças foram produzidas no dia ${num}? `))
+    producao.push(pecas)
+    num++
+}
+
+let total = 0
+for (let i = 0; i < producao.length; i++){
+    total += producao[i]
+}
+
+
+let media = total / producao.length
+
+let maior = producao[0]
+let menor = producao[0]
+for(let i = 1; i < producao.length; i++){
+    if(producao[i] > maior){
+        maior = producao[i]
+    }
+    if(producao[i] < menor){
+        menor = producao[i]
+    }
+}
+
+console.log (`--- RELATÓRIO DE PRODUÇÃO ---
+    Produções registradas: ${producao}
+    Total produzido: ${total}
+    Média diária: ${media}
+    Maior produção: ${maior}
+    Menor produção: ${menor}
+    Ordem crescente: ${producao.sort((a,b) => b-a)}
+    Ordem decrescente:${producao.sort((a,b) => a-b)}
+    `)
+    let metadeDoMaior = maior / 2
+
+    if (media >= metadeDoMaior) {
+        console.log("Produção estável!")
+    } else{
+        console.log("Produção abaixo do ideal.")
+    } 
