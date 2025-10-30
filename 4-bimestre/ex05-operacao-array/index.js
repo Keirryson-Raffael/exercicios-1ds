59ac98c55b821a2fe2e0cd6ea016abe409f184d1
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-10-30 13:48:16 -0300

    feito!

--- 4-bimestre/ex05-operacao-array/index.js
+++ 4-bimestre/ex05-operacao-array/index.js
@@ -1,4 +1,10 @@
 // CRIE UMA LÓGICA AQUI
+let compras = ["Sal", "Açucar", "Arroz"]
 
+compras.push("Feijão")
+compras.unshift("Farinha")
+compras.pop()
+
+console.log(compras)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { compras }
