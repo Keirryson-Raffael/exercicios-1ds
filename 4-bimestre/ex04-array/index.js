b9e08cf6fb26eb644ac3f8374c35a4e552434222
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-10-30 13:36:55 -0300

    feito!

--- 4-bimestre/ex04-array/index.js
+++ 4-bimestre/ex04-array/index.js
@@ -1,4 +1,8 @@
 // CRIE UMA LÓGICA AQUI
+let materias = ["Física", "Química", "Biologia", "Português"]
 
+console.log(materias[0])
+console.log(materias[3])
+console.log(materias.length)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { materias }
